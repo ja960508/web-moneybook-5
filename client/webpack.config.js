e220f0738465eb4e7d429c489f31cc82a83677c7
jooam <lord0325@naver.com>
2022-07-18 20:25:38 +0900

    conf: Webpack alias 설정
    - styles 경로 설정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,52 +5,59 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  // 파일을 읽어들이는 진입점 설정
+	// 파일을 읽어들이는 진입점 설정
 
-  entry: './src/js/index.js',
+	entry: './src/js/index.js',
 
-  // 결과물 반환 옵션
-  output: {
-    // 기본은 path dist, filename main.js
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    clean: true,
-    // 기존 빌드 결과 제거
-  },
+	// 결과물 반환 옵션
+	output: {
+		// 기본은 path dist, filename main.js
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		clean: true,
+		// 기존 빌드 결과 제거
+	},
 
-  devtool: 'eval-cheap-source-map',
+	devtool: 'eval-cheap-source-map',
 
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-          'sass-loader',
-        ],
-        exclude: /node_modules/,
-        // 순서 중요, 뒤에서부터 로드
-      },
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/,
-        type: 'asset/resource',
-      },
-    ],
-  },
+	resolve: {
+		modules: ['node_modules'],
+		alias: {
+			'@styles': path.resolve(__dirname, 'src/styles'),
+		},
+		extensions: ['.js'],
+	},
 
-  // 번들링 후 결과물의 처리 방식
-  plugins: [
-    new HtmlPlugin({
-      template: './index.html',
-    }),
-    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
-  ],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+				],
+				exclude: /node_modules/,
+				// 순서 중요, 뒤에서부터 로드
+			},
+			{
+				test: /\.js$/,
+				use: ['babel-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|svg|jpe?g|gif)$/,
+				type: 'asset/resource',
+			},
+		],
+	},
+
+	// 번들링 후 결과물의 처리 방식
+	plugins: [
+		new HtmlPlugin({
+			template: './index.html',
+		}),
+		new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+	],
 };
