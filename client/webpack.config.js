2e1e31303ea7e855be57c59b03d607a0b4cffa7e
jooam <lord0325@naver.com>
2022-07-18 16:31:15 +0900

    conf: 프로젝트 초기 설정
    - 번들링용 패키지 설치
    - 웹팩 config
    - 이슈, PR 템플릿 설정

---
+++ client/webpack.config.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+
+const HtmlPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  // 파일을 읽어들이는 진입점 설정
+
+  entry: './src/js/index.js',
+
+  // 결과물 반환 옵션
+  output: {
+    // 기본은 path dist, filename main.js
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    clean: true,
+    // 기존 빌드 결과 제거
+  },
+
+  devtool: 'eval-cheap-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+        exclude: /node_modules/,
+        // 순서 중요, 뒤에서부터 로드
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  // 번들링 후 결과물의 처리 방식
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+  ],
+};
