2f761f8155332e073f3eabac4d7236618103f31b
woochul2 <leewoo0686@gmail.com>
2022-07-19 18:45:01 +0900

    feat: Set reducer prototype

---
+++ client/src/js/store/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {};
+
+/**
+ * @param {object} state
+ * @param {object} action
+ * @param {string} action.type
+ * @param {*} action.payload
+ */
+export default function reducer(state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
