b7a72ea52bbf6f01e0bc4a3afd75f859587761fe
jooam <lord0325@naver.com>
2022-07-20 17:05:04 +0900

    feat: POST /api/paymentMethod API 템플릿 구현

--- server/src/api/payment_method/index.js
+++ server/src/api/payment_method/index.js
@@ -4,5 +4,6 @@ import * as ctrl from './payment_method.ctrl.js';
 const router = express.Router();
 
 router.get('/', ctrl.getPaymentMethods);
+router.post('/', ctrl.addPaymentMethod);
 
 export default router;
--- server/src/api/payment_method/payment_method.ctrl.js
+++ server/src/api/payment_method/payment_method.ctrl.js
@@ -3,3 +3,9 @@ import { createPaymentMethods } from '../../mock/mock_generator.js';
 export function getPaymentMethods(req, res) {
   res.status(200).json(createPaymentMethods());
 }
+
+export function addPaymentMethod(req, res) {
+  const { value } = req.body;
+
+  res.status(201).json({ id: 1 });
+}
