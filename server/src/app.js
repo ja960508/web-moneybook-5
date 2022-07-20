c2ef5d21ffe515d3450b90d0166cad1e119d4496
jooam <lord0325@naver.com>
2022-07-20 10:49:18 +0900

    feat: api Router init

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import apiRouter from './api/index.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
   console.log('app is listening on port ' + port);
