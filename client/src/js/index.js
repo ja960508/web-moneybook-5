1e373face0b747ea7554f0c97365774d9c2b658b
jooam <lord0325@naver.com>
2022-07-20 21:08:06 +0900

    feat: popstate 이벤트 발생 시 현재 active한 아이콘 변경

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -6,9 +6,11 @@ import Router from './core/router.js';
 import store from './store/store.js';
 import action from './store/action.js';
 import { getCurrentHistory } from './api/request.js';
+import Header from './components/Header.js';
 
 (async function () {
 	customElements.define('custom-link', CustomLink, { extends: 'a' });
+	const app = document.querySelector('#app');
 	const now = new Date();
 	const year = now.getFullYear();
 	const month = now.getMonth() + 1;
@@ -16,8 +18,17 @@ import { getCurrentHistory } from './api/request.js';
 	const response = await getCurrentHistory(year, month);
 	store.dispatch(action.getCurrentMonthData(response));
 
+	app.appendChild(new Header().DOMElement);
+	app.appendChild(document.createElement('main'));
 	window.addEventListener('popstate', () => {
-		Router.render(window.location.pathname);
+		const path = window.location.pathname;
+		Router.render(path);
+
+		const customLinks = document.querySelectorAll('nav [is=custom-link]');
+		customLinks.forEach((link) => {
+			link.classList.remove('active');
+			link.getAttribute('href') === path && link.classList.add('active');
+		});
 	});
 
 	renderHome();
