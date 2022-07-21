adc697d3498569dfced736ff66d4e6e5cc4a5ffb
jooam <lord0325@naver.com>
2022-07-21 21:47:15 +0900

    feat: 결제 수단 불러오기 인터페이스 및 SQL문 작성

--- server/src/db/DAO/payment_method_DAO.js
+++ server/src/db/DAO/payment_method_DAO.js
@@ -2,6 +2,16 @@ import pool from '../loader.js';
 
 const promisePool = pool.promise();
 
+export async function readPaymentMethod() {
+  try {
+    const res = await promisePool.execute(`SELECT * FROM PAYMENT_METHOD`);
+
+    return res[0];
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 export async function insertPaymentMethod(value) {
   try {
     const res = await promisePool.execute(
