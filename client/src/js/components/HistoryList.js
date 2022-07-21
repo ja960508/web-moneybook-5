3f30d54c7599d9f4bbd8e35c7c6e4d3cd6e016d4
jooam <lord0325@naver.com>
2022-07-21 11:12:48 +0900

    feat: 통계 탭에서 수입, 지출 총액을 나타내지 않기 위해 메소드 분리

--- client/src/js/components/HistoryList.js
+++ client/src/js/components/HistoryList.js
@@ -2,12 +2,28 @@ import icons from '../constants/icons';
 import store from '../store/store';
 
 class HistoryList {
-	constructor() {
+	constructor(props = {}) {
 		this.DOMElement = document.createElement('main');
+		this.props = props;
 		store.subscribe('history', this.render.bind(this));
 		store.subscribe('month', this.render.bind(this));
 
 		this.render();
+	}
+
+	getTotal(groupedHistory, day) {
+		return `
+					${
+						groupedHistory[day].incomeSum
+							? `수입 ${groupedHistory[day].incomeSum.toLocaleString()}`
+							: ''
+					}
+					${
+						groupedHistory[day].expenseSum
+							? `지출 ${groupedHistory[day].expenseSum.toLocaleString()}`
+							: ''
+					}
+				`;
 	}
 
 	template() {
@@ -47,18 +63,9 @@ class HistoryList {
 					(day) => `<li class="history__day bold-medium">
           <div class="history__day-meta">
             <div>${month}월 ${day}일</div>
-            <div class="history__day-total">
-              ${
-								groupedHistory[day].incomeSum
-									? `수입 ${groupedHistory[day].incomeSum.toLocaleString()}`
-									: ''
-							}
-              ${
-								groupedHistory[day].expenseSum
-									? `지출 ${groupedHistory[day].expenseSum.toLocaleString()}`
-									: ''
-							}
-            </div>
+						<div class="history__day-total">
+							${this.props.hideTotal ? '' : this.getTotal(groupedHistory, day)}
+						</div>
           </div>
         </li>`
 				)
