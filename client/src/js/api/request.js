cae45b04ee513289823dcfe30b0b1a220715317a
woochul2 <leewoo0686@gmail.com>
2022-07-25 20:54:46 +0900

    refactor: fetch 함수 공통되는 로직 함수로 분리

--- client/src/js/api/request.js
+++ client/src/js/api/request.js
@@ -1,10 +1,12 @@
 const API_ENDPOINT = 'http://localhost:3000/api';
 
-export async function getCurrentHistory(year, month) {
+/**
+ * @param {string} url
+ * @param {RequestInit} options
+ */
+async function fetchData(url, options) {
 	try {
-		const response = await fetch(
-			`${API_ENDPOINT}/history?year=${year}&month=${month}`
-		);
+		const response = await fetch(url, options);
 
 		if (!response.ok) {
 			throw new Error(`Request is not OK ${response.status}`);
@@ -12,91 +14,46 @@ export async function getCurrentHistory(year, month) {
 
 		return await response.json();
 	} catch (e) {
-		return e;
+		console.error(e);
 	}
+}
+
+export async function getCurrentHistory(year, month) {
+	return await fetchData(`${API_ENDPOINT}/history?year=${year}&month=${month}`);
 }
 
 export async function addHistory(history) {
-	try {
-		const response = await fetch(`${API_ENDPOINT}/history`, {
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-			body: JSON.stringify(history),
-		});
-
-		if (!response.ok) {
-			throw new Error(`Request is not OK ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (e) {
-		return e;
-	}
+	return await fetchData(`${API_ENDPOINT}/history`, {
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		method: 'POST',
+		body: JSON.stringify(history),
+	});
 }
 
 export async function getAllCategory() {
-	try {
-		const response = await fetch(`${API_ENDPOINT}/category`);
-
-		if (!response.ok) {
-			throw new Error(`Request is not OK ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (e) {
-		return e;
-	}
+	return await fetchData(`${API_ENDPOINT}/category`);
 }
 
 export async function getAllPaymentMethod() {
-	try {
-		const response = await fetch(`${API_ENDPOINT}/paymentMethod`);
-		if (!response.ok) {
-			throw new Error(`Request is not OK ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (e) {
-		return e;
-	}
+	return await fetchData(`${API_ENDPOINT}/paymentMethod`);
 }
 
 export async function addPaymentMethod(value) {
-	try {
-		const response = await fetch(`${API_ENDPOINT}/paymentMethod`, {
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-			body: JSON.stringify({ value }),
-		});
-
-		if (!response.ok) {
-			throw new Error(`Request is not OK ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (e) {
-		return e;
-	}
+	return await fetchData(`${API_ENDPOINT}/paymentMethod`, {
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		method: 'POST',
+		body: JSON.stringify({ value }),
+	});
 }
 
 export async function deletePaymentMethod(id) {
-	try {
-		const response = await fetch(`${API_ENDPOINT}/paymentMethod/${id}`, {
-			method: 'DELETE',
-		});
-
-		if (!response.ok) {
-			throw new Error(`Request is not OK ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (e) {
-		return e;
-	}
+	return await fetchData(`${API_ENDPOINT}/paymentMethod/${id}`, {
+		method: 'DELETE',
+	});
 }
