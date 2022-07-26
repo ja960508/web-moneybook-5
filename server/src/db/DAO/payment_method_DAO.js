a7b63d4adcacae6353f69e6c57c4edb15801571b
woochul2 <leewoo0686@gmail.com>
2022-07-26 09:52:02 +0900

    refactor: DAO에서 res를 구조분해할당해서 rows를 반환하도록 수정

--- server/src/db/DAO/payment_method_DAO.js
+++ server/src/db/DAO/payment_method_DAO.js
@@ -4,9 +4,9 @@ const promisePool = pool.promise();
 
 async function readPaymentMethod() {
   try {
-    const res = await promisePool.execute(`SELECT * FROM PAYMENT_METHOD`);
+    const [rows] = await promisePool.execute(`SELECT * FROM PAYMENT_METHOD`);
 
-    return res[0];
+    return rows;
   } catch (e) {
     console.error(e);
   }
@@ -14,12 +14,12 @@ async function readPaymentMethod() {
 
 async function insertPaymentMethod(value) {
   try {
-    const res = await promisePool.execute(
+    const [rows] = await promisePool.execute(
       `INSERT INTO PAYMENT_METHOD (name)
       VALUES ("${value}")`
     );
 
-    return res[0];
+    return rows;
   } catch (e) {
     console.error(e);
   }
@@ -27,12 +27,12 @@ async function insertPaymentMethod(value) {
 
 async function deletePaymentMethodById(id) {
   try {
-    const res = await promisePool.execute(`
+    const [rows] = await promisePool.execute(`
       DELETE FROM PAYMENT_METHOD
       WHERE id = ${id}
     `);
 
-    return res[0];
+    return rows;
   } catch (e) {
     console.error(e);
   }
